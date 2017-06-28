d56dc11aed4904ad47ef92ccd8f9810c99de974e
Tuong Minh Pham <phamtm@seagroup.com>
2017-06-28 19:02:18 +0800

    Remove react-lite branch

--- update-branches.sh
+++ update-branches.sh
@@ -3,5 +3,4 @@
 git checkout preact && git rebase master &&
 git checkout preact-compat && git rebase master &&
 git checkout react && git rebase master &&
-git checkout react-lite && git rebase master &&
 git checkout react-ts && git rebase master
