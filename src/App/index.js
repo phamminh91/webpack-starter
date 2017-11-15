605fcf28ca0339393b1102d548115e808277ac62
Tuong Minh Pham <phamtm@seagroup.com>
2017-11-15 18:29:37 +0800

    Add sprity and use babel loose mode

--- src/App/index.js
+++ src/App/index.js
@@ -1,5 +1,4 @@
 import { h } from 'preact';
-import Helmet from 'preact-helmet';
 import 'normalize.css/normalize.css';
 
 import './style.scss';
@@ -7,7 +6,6 @@ import './style.scss';
 export default function App() {
   return (
     <div className='container'>
-      <Helmet title='My PWA' />
       <h2>Img sprite</h2>
       <div className='app-icon' />
       <div className='lightning-icon' />
